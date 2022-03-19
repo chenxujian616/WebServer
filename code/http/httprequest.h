35de4ad5ed120d8c91c2debf99eb26ae559d72f0
chenxujian616 <13922606186@139.com>
2022-03-19 18:00:19 +0800

    httprequest

---
+++ code/http/httprequest.h
@@ -0,0 +1,78 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+// 正则表达式头文件
+#include <regex>
+#include <errno.h>
+#include <mysql/mysql.h>
+
+#include "../log/log.h"
+#include "../buffer/buffer.h"
+#include "../pool/sqlconnpool.h"
+#include "../pool/sqlconnRAII.h"
+
+class HttpRequest
+{
+public:
+    enum PARSE_STATE
+    {
+        REQUEST_LINE,
+        HEADERS,
+        BODY,
+        FINISH,
+    };
+
+    enum HTTP_CODE
+    {
+        NO_REQUEST = 0,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDENT_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION,
+    };
+
+private:
+    // 有点像是用于发送HTTP请求
+    bool ParseRequestLine_(const std::string &line);
+    bool ParseHeader_(const std::string &line);
+    bool ParseBody_(const std::string &line);
+
+    void ParsePath_(void);
+    void ParsePost_(void);
+    void ParseFromUrlencoded_(void);
+
+    static bool UserVerify(const std::string &name, const std::string &pwd, bool isLogin);
+
+    PARSE_STATE state_;
+    std::string method_, path_, version_, body_;
+    std::unordered_map<std::string, std::string> header_;
+    std::unordered_map<std::string, std::string> post_;
+
+    static const std::unordered_set<std::string> DEFAULT_HTML;
+    static const std::unordered_map<std::string, int> DEFAULT_HTML_TAG;
+    static int ConvertHex(char ch);
+
+public:
+    HttpRequest(/* args */);
+    ~HttpRequest();
+
+    void Init(void);
+    bool parse(Buffer &buff);
+
+    std::string path(void) const;
+    std::string &path(void);
+    std::string method(void) const;
+    std::string version(void) const;
+    std::string GetPost(const std::string &key) const;
+    std::string GetPost(const char *key) const;
+
+    bool IsKeepAlive(void) const;
+};
+
+#endif
